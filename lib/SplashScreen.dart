3fdb755fd83c0ddc7046130b79ed0b684d3fac85
youssef diaa <youssefdiaa22@icloud.com>
2023-09-25 18:09:37 +0300

    adding const in splash

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:islami_c9_sat/home/Settings/settings_data.dart';
 
 class SplashScreen extends StatelessWidget {
-   SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
