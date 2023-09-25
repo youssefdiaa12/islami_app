c09d899d6ee6b8f2870fb4035db85f131de6eb5d
youssef diaa <youssefdiaa22@icloud.com>
2023-09-25 17:51:27 +0300

    none1

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:islami_c9_sat/home/Settings/settings_data.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+   SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
