3633b365ba531c278a62aa36c63f99eb464b9a1b
youssef diaa <youssefdiaa22@icloud.com>
2023-10-05 14:50:59 +0300

    adding shared prefrence for theme & language

--- lib/provider/provider_theme_language.dart
+++ lib/provider/provider_theme_language.dart
@@ -1,24 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class provider_thene_language extends ChangeNotifier {
   ThemeMode currenttheme = ThemeMode.dark;
   String lang = 'English';
+  SharedPreferences? preferences;
 
   void changetheme(ThemeMode theme) {
     if (currenttheme == theme) {
+      SaveTheme(currenttheme);
       return;
     } else {
       currenttheme = theme;
+      SaveTheme(currenttheme);
       notifyListeners();
     }
   }
 
   void changeLanguage(String lan) {
     if (lang == lan) {
+      saveLanguage(lang);
       return;
     } else {
       lang = lan;
+      saveLanguage(lang);
       notifyListeners();
+    }
+  }
+
+  Future<void> SaveTheme(ThemeMode t) async {
+    String theme1 = t == ThemeMode.dark ? 'Dark' : 'Light';
+    await preferences?.setString('theme', theme1);
+  }
+
+  String? getTheme() {
+    return preferences?.getString('theme');
+  }
+
+  Future<void> loadTheme() async {
+    preferences = await SharedPreferences.getInstance();
+
+    String? theme = getTheme();
+    if (theme != null) {
+      theme == 'Dark'
+          ? currenttheme = ThemeMode.dark
+          : currenttheme = ThemeMode.light;
+    }
+  }
+
+  Future<void> saveLanguage(String lang) async {
+    await preferences?.setString('lang', lang);
+  }
+
+  String? getLanguage() {
+    return preferences?.getString('lang');
+  }
+
+  Future<void> loadLang() async {
+    preferences = await SharedPreferences.getInstance();
+    String? lang1 = preferences?.getString('lang');
+
+    if (lang1 != null) {
+      lang1 == 'English' ? lang = 'English' : lang = 'العربيه';
     }
   }
 }
